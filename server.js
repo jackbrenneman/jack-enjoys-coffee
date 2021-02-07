085363870dd768787862b6d349443acb85cd0305
Jack Brenneman <jbrenneman@wayfair.com>
2021-02-07 16:12:00 -0500

    testing some build stuff

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
