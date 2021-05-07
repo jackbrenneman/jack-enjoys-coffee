eca6788b8df6334dcea214135f74fd289b48806b
Jack Brenneman <jbrenneman@wayfair.com>
2021-05-07 14:11:17 -0400

    probably a simpler solution for any route

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
